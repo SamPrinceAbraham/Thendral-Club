73f3ae06afafe088b8f83265bff8a54b30e14223
Sam Prince <samprincecr7@gmail.com>
2025-12-02 13:35:27 +0530

    Simplify app.run by enabling debug mode only

--- app.py
+++ app.py
@@ -323,6 +323,4 @@ def admin_contacts():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    debug = os.environ.get('FLASK_ENV', '').lower() != 'production'
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(debug=True)
